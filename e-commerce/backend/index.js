f9c209468e20795008ca696108c9b47c588c7255
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-03 09:06:08 +0700

    finished connecting to mongodb and port 5000 now need to build database schema and model and connect in routes

--- e-commerce/backend/index.js
+++ e-commerce/backend/index.js
@@ -1,4 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://localhost/ecommerce',{ useUnifiedTopology: true, 
+useNewUrlParser: true },() => console.log('Connected to mongodb'))
+
+// Middlewares
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api',require('./routes/api'))
+
+
+app.get('/',(req,res) => res.send('Hello get'))
 
 app.listen(5000,() => console.log('Listening on port 5000...'))
